6de49232f8ada558037aaace9e86b65206800fba
aldm <d44.a1@yandex.ru>
2024-12-09 02:27:27 +0300

    1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 const dropButton = document.querySelector(".main__drop");
 const dropList = document.querySelector(".main__drop-list");
+const dropArrow = document.querySelector(".main__drop img");
 
 if (dropButton) {
   dropButton.addEventListener("click", () => {
     dropList.classList.toggle("active");
+    dropArrow.classList.toggle("active");
   });
 }
